4f8ce35802e5d809278ec6cb99fd1073006b7c55
Aniket Daripa <aniketdaripa84@gmail.com>
2023-06-27 20:18:59 +0530

    bill default add

--- server/model/bill.js
+++ server/model/bill.js
@@ -3,94 +3,117 @@ const mongoose = require("mongoose");
 const ReactBillDataSchema = new mongoose.Schema({
   userId: {
     type: String,
-    required:true
+    required:true,
+    default:"",
   },
   name: {
     type: String,
+    default:"",
     required:true
   },
   type: {
+    default:"",
     type: String,
     required:true,
   },
   pack: {
+    default:"",
     type: String,
     required:true
   },
   quantity: {
     type: String,
+    default:"",
     required:true
   },
   mrp: {
     type: String,
+    default:"",
     required:true
   },
   finalDiscount: {
     type: String,
+    default:"",
     required:true
   },
   gst: {
     type: String,
+    default:"",
     required:true
   },
   batchNo: {
     type: String,
+    default:"",
     required:true
   },
   exp: {
     type: String,
+    default:"",
     required:true
   },
   customerName: {
     type: String,
+    default:"",
     required:true
   },
   customerNumber: {
     type: String,
+    default:"",
     required:true
   },
   customerLocation: {
     type: String,
+    default:"",
     required:true
   },
   saleDate: {
     type: String,
+    default:"",
     required:true
   },
   diseaseType: {
     type: String,
+    default:"",
     required:true
   },
   loyaltyPoints: {
     type: String,
+    default:"",
     required:true
   },
   refillReminder: {
     type: String,
+    default:"",
     required:true
   },
   isPrescribed: {
     type: String,
+    default:"",
     required:true
   },
   billPreferance: {
+    default:"",
     type: String,
     required:true
   },
   doctorName: {
     type: String,
+    default:"",
     required:true
   },
   invoiceNumber: {
     type: String,
+    default:"",
     required:true
   },
   address: {
     type: String,
+    default:"",
     required:true
   },
   doctorDiscount: {
     type: String,
+    default:"",
     required:true
   },
 });
