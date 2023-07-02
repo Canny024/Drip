33f5fa6bdac051ef8c59276ab92335aea777a5d7
Abhayy <107317758+Canny024@users.noreply.github.com>
2023-07-02 16:18:29 +0530

    Update server.js

--- server.js
+++ server.js
@@ -45,11 +45,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 //serve static files
-app.use(express.static("./client/build"));
-app.get("*",(req,res)=>{
-  res.sendFile(path.join(__dirname,"client", "build", "index.html"));
-});
-//
+
  app.use("/", express.static(path.join(__dirname, "/public")));
 //app.use("/uploads",express.static("uploads"))
 
@@ -97,7 +93,11 @@ app.all("*", (req, res) => {
 app.use(errorHandler);
 
 // Production Script
-
+app.use(express.static("./client/build"));
+app.get("*",(req,res)=>{
+  res.sendFile(path.resolve(__dirname,"client", "build", "index.html"));
+});
+//
 
 
 mongoose.connection.once("open", () => {
