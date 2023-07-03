21c14a93e978c481832ea86034e36b5ff9c178d8
vasu dubey <134003663+autneo@users.noreply.github.com>
2023-07-03 20:09:26 +0530

    Update dbConn.js

--- config/dbConn.js
+++ config/dbConn.js
@@ -1,16 +1,15 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(
-      process.env.DATABASE_URI,
-      {
-        useUnifiedTopology: true,
-        useNewUrlParser: true,
-      }
-    );
-  } catch (err) {
-    console.error(err);
+    await mongoose.connect('mongodb://driperp-server:8D7hD9TFoGe4cRuE2j6z4450tWqBx5cmoSnvtWMNxPhtVn1k69ELvLlnM8tu66dCagxPzmZeNkTFACDbsyULkw==@driperp-server.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@driperp-server@', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('Connected to MongoDB');
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error);
   }
 };
+
 module.exports = connectDB;
