1f05f72dda658d9e4e7d33e71a59d8fa0c17f295
vasu dubey <134003663+autneo@users.noreply.github.com>
2023-07-03 20:31:56 +0530

    Update dbConn.js

--- config/dbConn.js
+++ config/dbConn.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://driperp-server:8D7hD9TFoGe4cRuE2j6z4450tWqBx5cmoSnvtWMNxPhtVn1k69ELvLlnM8tu66dCagxPzmZeNkTFACDbsyULkw==@driperp-server.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@driperp-server@', {
+    await mongoose.connect('mongodb://drip_login:u7ita5FOqEJe2KgX@ac-obzmmqx-shard-00-00.scakc63.mongodb.net:27017,ac-obzmmqx-shard-00-01.scakc63.mongodb.net:27017,ac-obzmmqx-shard-00-02.scakc63.mongodb.net:27017/?ssl=true&replicaSet=atlas-5cm5l3-shard-0&authSource=admin&retryWrites=true&w=majority', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
